2113d4077a8969217845b6edd2b081c6336bd898
Mateus Ferreira <mateusferreira0704@gmail.com>
2023-05-15 13:55:28 -0300

    Created Main Screen

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,13 +3,38 @@ import "./Input.scss";
 interface IInput {
   placeholder: string;
   label: string;
+  isTextArea?: boolean;
 }
 
 export const Input = (props: IInput) => {
   return (
     <div className="InputContainer">
-      <label htmlFor="input" className="label">{props.label}</label>
-      <input id="input" type="text" className="input" placeholder={props.placeholder} />
+      {props.isTextArea ? (
+        <>
+          <label htmlFor="textArea" className="label">
+            {props.label}
+          </label>
+          <textarea
+            id="textArea"
+            rows={4}
+            cols={50}
+            className="textArea"
+            placeholder={props.placeholder}
+          />
+        </>
+      ) : (
+        <>
+          <label htmlFor="input" className="label">
+            {props.label}
+          </label>
+          <input
+            id="input"
+            type="text"
+            className="input"
+            placeholder={props.placeholder}
+          />
+        </>
+      )}
     </div>
   );
 };
--- src/pages/mainScreen/MainScreen.tsx
+++ src/pages/mainScreen/MainScreen.tsx
@@ -1,9 +1,37 @@
+import { Card } from "../../components/card/Card";
 import "./MainScreen.scss";
+import { Button } from "../../components/button/Button";
+import { Input } from "../../components/input/Input";
 
 export const MainScreen = () => {
   return (
     <div className="MainScreen">
-      <div className="MainScreenContainer"></div>
+      <div className="mainScreenContainer">
+        <header>
+          <p>CodeLeap Network</p>
+        </header>
+        <main>
+          <article>
+            <Card
+              width={752}
+              title="What's on your mind?"
+              buttons={[
+                <Button text="Create" theme="Filled" disabled={false} />,
+              ]}
+            >
+              <Input
+                placeholder="Hello world"
+                label="Title"
+              />
+              <Input
+                isTextArea
+                placeholder="Content here"
+                label="Content"
+              />
+            </Card>
+          </article>
+        </main>
+      </div>
     </div>
   );
 };
